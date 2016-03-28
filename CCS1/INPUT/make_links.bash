f487621423cdee1d54597d6acb554c40fc6085b5
Raphael Dussin <raphael.dussin@gmail.com>
2016-03-28 19:12:52 -0400

    RD: links for input data

---
+++ CCS1/INPUT/make_links.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s /t3/workdir/forcings_sets/CORE2/lwrad.1948-2007.nc .
+ln -s /t3/workdir/forcings_sets/CORE2/Pair.1948-2007.nc .
+ln -s /t3/workdir/forcings_sets/CORE2/Qair.1948-2007.nc .
+ln -s /t3/workdir/forcings_sets/CORE2/rain.1948-2007.nc .
+ln -s /t3/workdir/forcings_sets/CORE2/swrad.1948-2007.nc .
+ln -s /t3/workdir/forcings_sets/CORE2/Tair.1948-2007.nc .
+ln -s /t3/workdir/forcings_sets/CORE2/Uwind.1948-2007.nc .
+ln -s /t3/workdir/forcings_sets/CORE2/Vwind.1948-2007.nc .
+ln -s /t3/workdir/raphael/WOA13-MOM6/WOA13-CM2.1_monthly.nc .
