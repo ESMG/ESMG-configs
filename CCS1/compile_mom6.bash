7b62fe77336e4d9c0025df816116bacb8829cf67
Raphael Dussin <raphael.dussin@gmail.com>
2016-03-28 18:42:46 -0400

    RD: cleanup compile script and add extras

--- CCS1/compile_mom6.bash
+++ CCS1/compile_mom6.bash
@@ -12,13 +12,6 @@ module load gcc/4.8.2
 module load netcdf/4.3.0-gcc4.8.2 
 module load openmpi/1.8.5_gcc4.8.2
 
-module list
-
-which gcc
-which mpif90
-which nc-config
-
-SRCDIR=$( pwd )
 cd $MOM6_rundir
 
 # Create blanl env file
@@ -48,14 +41,6 @@ fi
 cd $MOM6_rundir
 
 if [ $compile_mom == 1 ] ; then
-
-#   rm -Rf build/gnu/ocean_only/repro/
-#   mkdir -p build/gnu/ocean_only/repro/
-#   (cd build/gnu/ocean_only/repro/; rm -f path_names; \
-#   ../../../mkmf/bin/list_paths ./ ../../../../src/MOM6/{config_src/dynamic,config_src/solo_driver,src/{*,*/*}}/ ; \
-#   ../../../mkmf/bin/mkmf -t ../../../mkmf/templates/triton.mk -o '-I../../shared/repro' -p 'MOM6 -L../../shared/repro -lfms' -c "-Duse_libMPI -Duse_netCDF -DSPMD" path_names)
-#
-#   (cd build/gnu/ocean_only/repro/; source ../../env; make NETCDF=3 REPRO=1 FC=mpif90 LD=mpif90 MOM6 -j 6)
 
     rm -Rf build/gnu/ice_ocean_SIS2/repro/
     mkdir -p build/gnu/ice_ocean_SIS2/repro/
