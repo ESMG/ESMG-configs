a985696ad184aede0cc8549bf7b3416219c6d91e
Kate Hedstrom <kshedstrom@alaska.edu>
2018-04-13 14:56:00 -0800

    Add v to variables written by extract_obc.

--- ocean_only/dumbbell/z/extract_obc.bash
+++ ocean_only/dumbbell/z/extract_obc.bash
@@ -5,6 +5,7 @@ ncks -O -d xq,41,41 -v u prog_late.nc west.nc
 ncks -d xh,41,41 -v SSH -A  prog_late.nc west.nc
 ncks -d xh,41,41 -v salt -A  prog_late.nc west.nc
 ncks -d xh,41,41 -v h -A  prog_late.nc west.nc
+ncks -d xh,41,41 -v v -A  prog_late.nc west.nc
 ncatted -a modulo,Time,c,c,' ' west.nc
 ncrename -v u,u_segment_001 west.nc
 ncrename -v SSH,zeta_segment_001 west.nc
@@ -12,11 +13,15 @@ ncrename -v h,dz_u_segment_001 west.nc
 ncks -d xh,41,41 -v h -A  prog_late.nc west.nc
 ncrename -v h,dz_salt_segment_001 west.nc
 ncrename -v salt,salt_segment_001 west.nc
+ncrename -v v,v_segment_001 west.nc
+ncks -d xh,41,41 -v h -A  prog_late.nc west.nc
+ncrename -v h,dz_v_segment_001 west.nc
 
 ncks -O -d xq,81,81 -v u prog_late.nc east.nc
 ncks -d xh,82,82 -v SSH -A  prog_late.nc east.nc
 ncks -d xh,82,82 -v salt -A  prog_late.nc east.nc
 ncks -d xh,82,82 -v h -A  prog_late.nc east.nc
+ncks -d xh,82,82 -v v -A  prog_late.nc east.nc
 ncatted -a modulo,Time,c,c,' ' east.nc
 ncrename -v u,u_segment_002 east.nc
 ncrename -v SSH,zeta_segment_002 east.nc
@@ -24,3 +29,6 @@ ncrename -v h,dz_u_segment_002 east.nc
 ncks -d xh,82,82 -v h -A  prog_late.nc east.nc
 ncrename -v h,dz_salt_segment_002 east.nc
 ncrename -v salt,salt_segment_002 east.nc
+ncrename -v v,v_segment_002 east.nc
+ncks -d xh,82,82 -v h -A  prog_late.nc east.nc
+ncrename -v h,dz_v_segment_002 east.nc
