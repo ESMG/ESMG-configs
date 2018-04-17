c3c0926f3ae74caffb5541076f4bee893eaf5b88
Kate Hedstrom <kshedstrom@alaska.edu>
2018-04-17 15:28:42 -0800

    Fix dz for v in extract_obc script.

--- ocean_only/dumbbell/z/extract_obc.bash
+++ ocean_only/dumbbell/z/extract_obc.bash
@@ -11,11 +11,12 @@ ncrename -v u,u_segment_001 west.nc
 ncrename -v SSH,zeta_segment_001 west.nc
 ncrename -v h,dz_u_segment_001 west.nc
 ncks -d xh,41,41 -v h -A  prog_late.nc west.nc
-ncrename -v h,dz_salt_segment_001 west.nc
 ncrename -v salt,salt_segment_001 west.nc
 ncrename -v v,v_segment_001 west.nc
-ncks -d xh,41,41 -v h -A  prog_late.nc west.nc
-ncrename -v h,dz_v_segment_001 west.nc
+ncks -d xh,41,41 -v v -A  prog_late.nc west.nc
+ncap2 -A -s 'v(:,:,0,:)=0; v(:,:,20,:)=0; v(:,:,1:19,:)=0.5*(h(:,:,0:18,:)+h(:,:,1:19,:))' west.nc west.nc
+ncrename -v h,dz_salt_segment_001 west.nc
+ncrename -v v,dz_v_segment_001 west.nc
 
 ncks -O -d xq,81,81 -v u prog_late.nc east.nc
 ncks -d xh,82,82 -v SSH -A  prog_late.nc east.nc
@@ -27,8 +28,9 @@ ncrename -v u,u_segment_002 east.nc
 ncrename -v SSH,zeta_segment_002 east.nc
 ncrename -v h,dz_u_segment_002 east.nc
 ncks -d xh,82,82 -v h -A  prog_late.nc east.nc
-ncrename -v h,dz_salt_segment_002 east.nc
 ncrename -v salt,salt_segment_002 east.nc
 ncrename -v v,v_segment_002 east.nc
-ncks -d xh,82,82 -v h -A  prog_late.nc east.nc
-ncrename -v h,dz_v_segment_002 east.nc
+ncks -d xh,82,82 -v v -A  prog_late.nc east.nc
+ncap2 -A -s 'v(:,:,0,:)=0; v(:,:,20,:)=0; v(:,:,1:19,:)=0.5*(h(:,:,0:18,:)+h(:,:,1:19,:))' east.nc east.nc
+ncrename -v h,dz_salt_segment_002 east.nc
+ncrename -v v,dz_v_segment_002 east.nc
